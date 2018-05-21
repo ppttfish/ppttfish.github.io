49857cae675b20d940424c938bc5c8a7c9333a26
ppttfish <ppttfish@163.com>
2018-05-21 13:27:48 +0800

    Site updated: 2018-05-21 13:27:47

--- js/index.js
+++ js/index.js
@@ -5,26 +5,39 @@
   var searchModal = document.getElementsByClassName('search-dialog')[0];
   var closeBtn = document.getElementsByClassName('modal-close');
   var returnTop = document.getElementById('return-top');
+  var navContainer = document.getElementsByClassName('nav-container')[0];
   
   // 回到顶部
   window.onscroll = function() {
     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     returnTop.style.display = scrollTop > 500 ? 'block' : 'none';
+    var windowWidth = window.screen.availWidth;
+
+    if (windowWidth < 1200) {
+      if (scrollTop >= 522) {
+        navContainer.style.background = '#fff';
+        navContainer.style.color = 'black';
+      }
+      if (scrollTop < 522 ) {
+        navContainer.style.background = 'transparent';
+      }
+    }
   };
 
   returnTop.onclick = function () {
-    scrollToptimer = setInterval(function () {
-      var top = document.body.scrollTop || document.documentElement.scrollTop;
-      var speed = top / 1.5;
-      if (document.body.scrollTop!=0) {
-          document.body.scrollTop -= speed;
-      }else {
-          document.documentElement.scrollTop -= speed;
-      }
-      if (top == 0) {
-          clearInterval(scrollToptimer);
-      }
-    }, 30); 
+    // scrollToptimer = setInterval(function () {
+    //   var top = document.body.scrollTop || document.documentElement.scrollTop;
+    //   var speed = top / 1.5;
+    //   if (document.body.scrollTop!=0) {
+    //       document.body.scrollTop -= speed;
+    //   }else {
+    //       document.documentElement.scrollTop -= speed;
+    //   }
+    //   if (top == 0) {
+    //       clearInterval(scrollToptimer);
+    //   }
+    // }, 30); 
+    $("html, body").animate({ scrollTop: 0 }, 120);
   }
 
 
